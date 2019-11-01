374c454f3a2cefd60409e4eea62c74d13244f98a
Emily_Richard <richard.emily.e@gmail.com>
2019-11-01 12:45:48 -0400

    finished projectRouter

--- actionRouter.js
+++ actionRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const projectDB = require('./data/helpers/projectModel');
+const db = require('./data/helpers/projectModel');
 
 module.exports = router;
